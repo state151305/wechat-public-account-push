21376e8ed9183eb249a2eadb842c58efd8986596
state151305 <97270322+state151305@users.noreply.github.com>
2022-11-07 10:30:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyQfI6AIBjxFR_sxoFWA8ACcVcrI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Nc7D57e6zXIythNOyTLJIGw1lcpU2zhi5jeGGRwCpa4',
+      useTemplateId: 'yuA3tLk_zaDiO_TecvU0szZzgqSD9xSrm89PG5mK3tc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-03',
       festivals: [
@@ -34,9 +34,6 @@ const USER_CONFIG = {
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '宝贝', year: '2001', date: '09-03',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2022', date: '04-30',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
